f50ecd4942c0c89ec6d0cb981e5b031505bd28cc
ryugakune-rikuya <ev50516@meiji.ac.jp>
2019-10-22 20:54:37 +0900

    [fix] fix access path

--- index.js
+++ index.js
@@ -50,6 +50,8 @@ app.use(express.static(__dirname + "/dist/"))
 app.get(/.*/, function (req, res) {
   res.sendfile(__dirname + "/dist/index.html")
 })
-
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + "/dist/index.html")
+})
 app.listen(port)
 console.log('listening')
